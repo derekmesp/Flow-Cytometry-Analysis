95aaa7d3bcdcac07d4ee63cfaf153fa1f2e4105f
derekmesp <dam2275@columbia.edu>
2026-07-23 15:30:33 -0400

    pep-8 formatting for repository

--- src/plotting_methods.py
+++ src/plotting_methods.py
@@ -2,14 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.manifold import TSNE
 
+
 def pca_plot(grouped_pca, tissue_type, sample_name):
     """
     Creates a Principal Component Analysis (PCA) scatter plot with samples grouped by donor type.
-    
+
     This function visualizes PCA results by plotting PC1 vs PC2, with each point representing a sample.
     Samples are color-coded by donor group, labeled with their IDs, and encircled by group-specific
     boundaries. The plot includes a legend identifying each donor group.
-    
+
     Parameters
     ----------
     grouped_pca : pandas.DataFrame
@@ -19,22 +20,24 @@ def pca_plot(grouped_pca, tissue_type, sample_name):
         The type of tissue being analyzed, used in the plot title.
     sample_name : str
         The name or identifier of the sample set, used in the plot title.
-        
+
     Returns
     -------
     None
         The function displays the plot but does not return any value.
     """
     donor_colors = {
-    'ctr': 'grey',
-    'hst': 'pink',
-    'ftl': 'red'
+        'ctr': 'grey',
+        'hst': 'pink',
+        'ftl': 'red'
     }
 
-    colors = grouped_pca['donor_group'].map(lambda x: donor_colors.get(x, 'black'))
+    colors = grouped_pca['donor_group'].map(
+        lambda x: donor_colors.get(x, 'black'))
 
     plt.figure(figsize=(8, 6))
-    plt.scatter(grouped_pca['PC1'], grouped_pca['PC2'], c=colors, edgecolor='k', s=100)
+    plt.scatter(grouped_pca['PC1'], grouped_pca['PC2'],
+                c=colors, edgecolor='k', s=100)
 
     for sample_id, (x, y) in grouped_pca[['PC1', 'PC2']].iterrows():
         plt.text(x, y, sample_id, fontsize=9)
@@ -43,30 +46,34 @@ def pca_plot(grouped_pca, tissue_type, sample_name):
         subset = grouped_pca[grouped_pca['donor_group'] == group]
         if not subset.empty:
             centroid_x, centroid_y = subset[['PC1', 'PC2']].mean()
-            max_distance = np.max(np.linalg.norm(subset[['PC1', 'PC2']].values - np.array([centroid_x, centroid_y]), axis=1))
-            circle = plt.Circle((centroid_x, centroid_y), max_distance, color=color, fill=False, linestyle='--', linewidth=2)
+            max_distance = np.max(np.linalg.norm(
+                subset[['PC1', 'PC2']].values - np.array([centroid_x, centroid_y]), axis=1))
+            circle = plt.Circle((centroid_x, centroid_y), max_distance,
+                                color=color, fill=False, linestyle='--', linewidth=2)
             plt.gca().add_patch(circle)
 
     plt.xlabel('PC1')
     plt.ylabel('PC2')
     plt.title('{} {} PCA'.format(tissue_type, sample_name))
 
-    handles = [plt.Line2D([0], [0], marker='o', color=color, linestyle='', markersize=10) 
-            for color in donor_colors.values()]
-    plt.legend(handles, donor_colors.keys(), title='Donor Group', bbox_to_anchor=(1.05, 1), loc='upper left')
+    handles = [plt.Line2D([0], [0], marker='o', color=color, linestyle='', markersize=10)
+               for color in donor_colors.values()]
+    plt.legend(handles, donor_colors.keys(), title='Donor Group',
+               bbox_to_anchor=(1.05, 1), loc='upper left')
 
     plt.tight_layout()
     plt.show()
-    
+
+
 def tSNE_plot(grouped_pca, tissue_type, sample_name):
     """
     Creates a t-Distributed Stochastic Neighbor Embedding (t-SNE) scatter plot with samples grouped by donor type.
-    
+
     This function applies t-SNE dimensionality reduction to the input data and visualizes the results
     by plotting tSNE1 vs tSNE2, with each point representing a sample. Samples are color-coded by donor 
     group, labeled with their IDs, and encircled by group-specific boundaries. The plot includes a legend 
     identifying each donor group.
-    
+
     Parameters
     ----------
     grouped_pca : pandas.DataFrame
@@ -76,13 +83,13 @@ def tSNE_plot(grouped_pca, tissue_type, sample_name):
         The type of tissue being analyzed, used in the plot title.
     sample_name : str
         The name or identifier of the sample set, used in the plot title.
-        
+
     Returns
     -------
     None
         The function displays the plot but does not return any value.
     """
-    tsne = TSNE(n_components=2, random_state=0, perplexity = 5)
+    tsne = TSNE(n_components=2, random_state=0, perplexity=5)
     tsne_results = tsne.fit_transform(grouped_pca.iloc[:, :-1])
 
     grouped_pca['tSNE1'] = tsne_results[:, 0]
@@ -94,10 +101,12 @@ def tSNE_plot(grouped_pca, tissue_type, sample_name):
         'ftl': 'red'
     }
 
-    colors = grouped_pca['donor_group'].map(lambda x: donor_colors.get(x, 'black'))
+    colors = grouped_pca['donor_group'].map(
+        lambda x: donor_colors.get(x, 'black'))
 
     plt.figure(figsize=(8, 6))
-    plt.scatter(grouped_pca['tSNE1'], grouped_pca['tSNE2'], c=colors, edgecolor='k', s=100)
+    plt.scatter(grouped_pca['tSNE1'], grouped_pca['tSNE2'],
+                c=colors, edgecolor='k', s=100)
 
     for sample_id, (x, y) in grouped_pca[['tSNE1', 'tSNE2']].iterrows():
         plt.text(x, y, sample_id, fontsize=9)
@@ -106,17 +115,20 @@ def tSNE_plot(grouped_pca, tissue_type, sample_name):
         subset = grouped_pca[grouped_pca['donor_group'] == group]
         if not subset.empty:
             centroid_x, centroid_y = subset[['tSNE1', 'tSNE2']].mean()
-            max_distance = np.max(np.linalg.norm(subset[['tSNE1', 'tSNE2']].values - np.array([centroid_x, centroid_y]), axis=1))
-            circle = plt.Circle((centroid_x, centroid_y), max_distance, color=color, fill=False, linestyle='--', linewidth=2)
+            max_distance = np.max(np.linalg.norm(
+                subset[['tSNE1', 'tSNE2']].values - np.array([centroid_x, centroid_y]), axis=1))
+            circle = plt.Circle((centroid_x, centroid_y), max_distance,
+                                color=color, fill=False, linestyle='--', linewidth=2)
             plt.gca().add_patch(circle)
 
     plt.xlabel('tSNE1')
     plt.ylabel('tSNE2')
     plt.title('{} {} t-SNE'.format(tissue_type, sample_name))
 
-    handles = [plt.Line2D([0], [0], marker='o', color=color, linestyle='', markersize=10) 
-            for color in donor_colors.values()]
-    plt.legend(handles, donor_colors.keys(), title='Donor Group', bbox_to_anchor=(1.05, 1), loc='upper left')
+    handles = [plt.Line2D([0], [0], marker='o', color=color, linestyle='', markersize=10)
+               for color in donor_colors.values()]
+    plt.legend(handles, donor_colors.keys(), title='Donor Group',
+               bbox_to_anchor=(1.05, 1), loc='upper left')
 
     plt.tight_layout()
     plt.show()
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,24 +1,25 @@
-import flowkit as fk 
+import flowkit as fk
 import pandas as pd
 import scanpy as sc
 import matplotlib.pyplot as plt
 from difflib import SequenceMatcher
 
+
 def read_flow(directory, tissue=None):
     """
     Reads flow cytometry data from FCS files in the specified directory.
-    
+
     This function loads flow cytometry data from FCS files, processes them into a pandas DataFrame,
     and assigns tissue and condition labels based on sample IDs. It automatically detects control
     (ctr), history (hst), and fatal (ftl) asthmatic conditions from the sample names.
-    
+
     Parameters:
     -----------
     directory : str
         Path to the directory containing FCS files to be analyzed.
     tissue : str, optional
         Tissue type label to assign to all samples. If None, no tissue label is assigned.
-        
+
     Returns:
     --------
     tuple
@@ -30,7 +31,7 @@ def read_flow(directory, tissue=None):
     """
     session = fk.Session(fcs_samples=directory)
     sample_list = session.get_sample_ids()
-    
+
     df_flow = []
     for sample_id in sample_list:
         df = session.get_gate_events(sample_id)
@@ -52,14 +53,17 @@ def read_flow(directory, tissue=None):
                 f"Sample {sample_id} missing condition annotation. Insert manually: "
             )
             df["condition"] = manual_condition
-            df["asthma"] = "asthmatic" if manual_condition in ["hst", "ftl"] else "control"
-            
+            df["asthma"] = "asthmatic" if manual_condition in [
+                "hst", "ftl"] else "control"
+
         df_flow.append(df)
-     
+
     df_flow = pd.concat(df_flow)
-    df_flow.columns = [pns if pns != '' else pnn for pnn, pns in df_flow.columns]
+    df_flow.columns = [pns if pns !=
+                       '' else pnn for pnn, pns in df_flow.columns]
     print('Parameters:', df_flow.keys())
     return df_flow, sample_list, session
+
 
 def automate_merging(df, columns, threshold=0.6):
     """
@@ -72,32 +76,32 @@ def automate_merging(df, columns, threshold=0.6):
         A DataFrame containing flow cytometry data, containing duplicate channels.
     columns : list
         List containing the columns that are duplicate channels.
-    
+
     Returns:
     --------
     pandas.DataFrame
         Dataframe containing flow cytometry data with merged channels and excluding duplicates.
-    
+
     """
     remaining = set(columns)
-    
+
     while remaining:
         s1 = remaining.pop()
-        matches = [(s2, SequenceMatcher(None, s1, s2).ratio()) for s2 in remaining]
-        
+        matches = [(s2, SequenceMatcher(None, s1, s2).ratio())
+                   for s2 in remaining]
+
         if matches:
             best_match, score = max(matches, key=lambda x: x[1])
-            
+
             if score >= threshold:
                 df[s1] = df[[s1, best_match]].max(axis=1)
                 print(f"Merged {best_match} into {s1} (Score: {score:.2f})")
-                
+
                 remaining.remove(best_match)
                 df.drop(columns=[best_match], inplace=True)
-                
+
     return df
-    
-    
+
 
 def pd_to_adata(df_flow, df_flow_counts):
     """
@@ -126,7 +130,7 @@ def pd_to_adata(df_flow, df_flow_counts):
     if df_flow_counts.isna().any().any():
         nan_cols = df_flow_counts.columns[df_flow_counts.isna().any()].tolist()
         df_flow_counts = automate_merging(df_flow_counts, nan_cols)
-        
+
     df_flow['sample_id'] = df_flow['sample_id'].apply(lambda x: x[:4])
     list_metadata = {
         'group': df_flow.condition,
@@ -146,14 +150,16 @@ def pd_to_adata(df_flow, df_flow_counts):
         adata.obs[col] = df_metadata[col]
 
     return adata
+
+
 def pca_df(sample, session=None, singular=True, sample_list=None):
     """
     Creates a DataFrame of PCA values from flow cytometry data, grouped by sample ID.
-    
+
     This function extracts PCA coordinates from an AnnData object, groups them by sample ID,
     and adds donor group information based on sample IDs. It can work with either a single
     sample or multiple samples.
-    
+
     Parameters:
     -----------
     sample : anndata.AnnData
@@ -165,7 +171,7 @@ def pca_df(sample, session=None, singular=True, sample_list=None):
         If True, retrieves sample IDs from the session. If False, uses the provided sample_list.
     sample_list : list, optional
         List of sample IDs to use when singular=False.
-        
+
     Returns:
     --------
     pandas.DataFrame
@@ -173,18 +179,20 @@ def pca_df(sample, session=None, singular=True, sample_list=None):
         - PC1, PC2, ...: Principal component coordinates
         - donor_group: Donor group information extracted from sample IDs
     """
-    
+
     if singular:
         sample_list = session.get_sample_ids()
-        
+
     sample_dict = {}
     for sample_id in sample_list:
-        sample_dict[sample_id[:4]] = 'ctr' if 'ctr' in sample_id else 'hst' if 'hst' in sample_id else 'ftl'
-    
-    pca_df = pd.DataFrame(sample.obsm['X_pca'], columns=[f'PC{i+1}' for i in range(sample.obsm['X_pca'].shape[1])])
+        sample_dict[sample_id[:4]
+                    ] = 'ctr' if 'ctr' in sample_id else 'hst' if 'hst' in sample_id else 'ftl'
+
+    pca_df = pd.DataFrame(sample.obsm['X_pca'], columns=[
+                          f'PC{i+1}' for i in range(sample.obsm['X_pca'].shape[1])])
     pca_df['sample_id'] = sample.obs['sample_id'].values
-    
+
     grouped_pca = pca_df.groupby('sample_id').mean()
     grouped_pca['donor_group'] = grouped_pca.index.map(sample_dict)
-    
+
     return grouped_pca
